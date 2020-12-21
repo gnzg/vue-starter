7677b46e941c9235d97828b62810385ac44adb99
gnzg <24455612+gnzg@users.noreply.github.com>
2020-12-21 22:07:45 +0100

    ADD: 3 breakpoints and mixin for a responsive layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="container">
+      <img alt="Vue logo" src="./assets/logo.png">
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,6 +19,22 @@ export default {
 </script>
 
 <style lang="scss">
+
+$breakpoints: (
+  'mobile': 576px,
+  'tablet':  768px,
+  'desktop': 992px
+);
+
+@mixin respond-to($breakpoint) {
+  @if map-has-key($breakpoints, $breakpoint) {
+    @media (min-width: map-get($breakpoints, $breakpoint)) {
+      @content;
+    }
+  }
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +42,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  .container {
+    background-color: red;
+    @include respond-to('tablet') {
+      background-color: yellow;
+    }
+    @include respond-to('desktop') {
+      background-color: green;
+    }
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+  }
 }
 </style>
